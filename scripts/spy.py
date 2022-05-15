c51cfe9c2f5872dfa1c93965636ac1f90769dfcc
cstyl <cstyl16@gmail.com>
2022-05-15 09:09:59 +0100

    updating script for plotting sparsity pattern to also generate the coarse matrices

--- scripts/spy.py
+++ scripts/spy.py
@@ -2,7 +2,20 @@ import numpy as np
 from scipy import sparse as sp
 import matplotlib.pylab as plt
 import argparse
-import os
+
+
+def build_matrix(filename):
+    mat = np.genfromtxt(filename, delimiter=" ")
+
+    # split matrix to I, J, V
+    I, J, V = np.hsplit(mat, 3)
+    II = I.reshape(I.shape[0]).astype(int)
+    JJ = J.reshape(J.shape[0]).astype(int)
+    VV = V.reshape(V.shape[0])
+
+    # convert to CSR
+    return sp.csr_matrix((VV, (II, JJ)))
+
 
 # get filename to process
 parser = argparse.ArgumentParser(
@@ -10,10 +23,10 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument(
-    "--filename",
+    "--expdir",
     type=str,
     required=True,
-    help="Absolute filename to the Matrix file to be processed.",
+    help="Absolute directory to the experiment containing the matrices to be processed.",
 )
 
 parser.add_argument(
@@ -22,36 +35,38 @@ parser.add_argument(
     default=4,
     help="Number of processes to be split across.",
 )
+
+parser.add_argument(
+    "--nlevels",
+    type=int,
+    default=3,
+    help="Number of MG levels.",
+)
 args = parser.parse_args()
 
-filedir = os.path.dirname(args.filename)
 file_extension = "jpg"
-outfile = filedir + "/plot." + file_extension
 
 print("*" * 10, " spy.py ", "*" * 10)
-print("Input File  : ", args.filename)
-print("Output Plot : ", outfile)
-print("Decomposing over : ", args.nprocs, " process(es)")
+print("Experiment Directory : ", args.expdir)
+print("Decomposing over     : ", args.nprocs, " process(es)")
+print("Number of MG Levels  : ", args.nlevels)
 print("*" * 30)
 
-# read matrix from A.dat (file produced during HPCG run with detail debug on)
-matrix = np.genfromtxt(args.filename, delimiter=" ")
+matrices = {"A": build_matrix(args.expdir + "/matrix-0.txt")}
+for lvl in range(1, args.nlevels):
+    matrices["Ac" + str(lvl)] = build_matrix(
+        args.expdir + "/coarse-" + str(lvl) + "-matrix-0.txt"
+    )
 
-# split matrix to I, J, V
-I, J, V = np.hsplit(matrix, 3)
-II = I.reshape(I.shape[0]).astype(int)
-JJ = J.reshape(J.shape[0]).astype(int)
-VV = V.reshape(V.shape[0])
+for key, mat in matrices.items():
+    # plot
+    fig, ax = plt.subplots(tight_layout=True)
+    plt.spy(mat, markersize=1)
+    Acsr_shape = mat.get_shape()
 
-# convert to CSR
-Acsr = sp.csr_matrix((VV, (II, JJ)))
-
-# plot
-fig, ax = plt.subplots(tight_layout=True)
-plt.spy(Acsr, markersize=1)
-Acsr_shape = Acsr.get_shape()
-
-for proc in range(args.nprocs):
-    plt.axhline(y=Acsr_shape[0] * proc / args.nprocs, color="r", linestyle="--")
-    plt.axvline(x=Acsr_shape[1] * proc / args.nprocs, color="r", linestyle="--")
-fig.savefig(outfile, format=file_extension)
+    for proc in range(args.nprocs):
+        plt.axhline(y=Acsr_shape[0] * proc / args.nprocs, color="r", linestyle="--")
+        plt.axvline(x=Acsr_shape[1] * proc / args.nprocs, color="r", linestyle="--")
+    fig.savefig(
+        args.expdir + "/plot-" + key + "." + file_extension, format=file_extension
+    )
